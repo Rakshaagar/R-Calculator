a81011a9eb3ffd9e7943364c86682e92e171a8c0
Rakshaagar saumiyamoorthy <124656658+Rakshaagar@users.noreply.github.com>
2023-09-07 15:54:15 +0530

    Update Calculator.js

--- Calculator.js
+++ Calculator.js
@@ -1,1 +1,25 @@
+function display(val){
+    document.getElementById('result').value+=val
+    return val
 
+
+}
+
+function display(val){
+
+    document.getElementById('result').value-=val
+    return val
+
+}
+
+function display(val){
+
+    document.getElementById('result').value*=val
+    return val
+
+}
+
+function display(val){
+    document.getElementById('result').value/=val
+    return val
+}
